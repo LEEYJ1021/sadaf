a85da1fc3983b6dca0a9cb9129e5f3bd9681774c
Yong-Jae Lee <yongjaelee1021@gmail.com>
2026-06-16 17:57:23 +0900

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,9 @@
+"""
+SADAF: Sparse Ad Data Augmentation Framework
+============================================
+A unified causal-predictive-explainable pipeline for cold-start
+advertisement performance forecasting.
+"""
+
+__version__ = "1.0.0"
+__author__ = "SADAF Authors"
